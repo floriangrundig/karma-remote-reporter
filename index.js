bf501d714639b93d110e8ca2296e90ee04dd93c9
ohoogvliet <oliver@hoogvliet.de>
2013-08-16 16:15:13 +0200

    Progress from a common testing session: improved code of index.js; testwebsocketserver can handle messages (IN PROGRESS)

--- index.js
+++ index.js
@@ -3,102 +3,94 @@ var WebSocketClient = require('websocket').client;
 
 
 var RemoteReporter = function (baseReporterDecorator, config, emitter, logger, helper, formatError) {
-    var log = logger.create('reporter.remote');
-    var clientUrl = "ws://" + config.host + ":" + config.port;
+  var log = logger.create('reporter.remote');
+  var clientUrl = "ws://" + config.host + ":" + config.port;
 
-    var client = new WebSocketClient();
-    client.connect(clientUrl, 'karma-test-results'); // TODO: wait until connected, because the reporter could be called by karma before the connection process finished
+  var client = new WebSocketClient();
+  client.connect(clientUrl, 'karma-test-results'); // TODO: wait until connected, because the reporter could be called by karma before the connection process finished
 
-    var connection;
+  var connection;
 
-    client.on('connect', function (con) {
-        connection = con;
+  client.on('connect', function (con) {
+    connection = con;
+  });
+
+  var savedBrowsers = [];
+  var allMessages = [];
+
+  baseReporterDecorator(this);
+
+  this.adapters = [function (msg) {
+    allMessages.push(msg);
+  }];
+
+
+  client.on('connectFailed', function (error) {
+    console.log('Connect Error: ' + error.toString());
+  });
+
+  function sendData(objectToSend) {
+    if (connection.connected) {
+      connection.sendUTF(JSON.stringify(objectToSend));
+    } else {
+      log.error("Failed to use connection")
+    }
+  }
+
+  function getCurrentTimestamp() {
+    return (new Date()).toISOString().substr(0, 19);
+  }
+
+  this.onRunStart = function (browsers) {
+
+    log.info('WebSocket client connected');
+    connection.on('error', function (error) {
+      log.info("Connection Error: " + error.toString());
     });
 
-    var browsers2 = {};
-    var allMessages = [];
-
-    baseReporterDecorator(this);
-
-    this.adapters = [function (msg) {
-        allMessages.push(msg);
-    }];
-
-
-    client.on('connectFailed', function (error) {
-        console.log('Connect Error: ' + error.toString());
+    connection.on('close', function () {
+      log.info(' Connection Closed');
     });
 
+    var timestamp = getCurrentTimestamp();
 
-    this.onRunStart = function (browsers) {
+    browsers.forEach(function (browser) {
+      savedBrowsers.push({
+        name: browser.name, timestamp: timestamp, browserId: browser.id, hostname: os.hostname()
+      });
+      log.info("onRunStart: " + savedBrowsers[browser.id]);
+    });
 
-        log.info('WebSocket client connected');
-            connection.on('error', function (error) {
-                log.info("Connection Error: " + error.toString());
-            });
-            connection.on('close', function () {
-                log.info(' Connection Closed');
-            });
+    sendData({ "type": "browsers", "list": savedBrowsers});
+  };
 
+  this.onBrowserComplete = function (browser) {
+    sendData({ "type": "browserComplete", 'browserId': browser.id, 'timestamp': getCurrentTimestamp()});
+    log.info("onBrowserComplete: " + browser);
+  };
 
-            var timestamp = (new Date()).toISOString().substr(0, 19);
-
-            browsers.forEach(function (browser) {
-                browsers2[browser.id] = {
-                    name: browser.name, timestamp: timestamp, id: 0, hostname: os.hostname()
-                };
-
-
-                log.info("onRunStart: " + browsers2[browser.id]);
-            });
-
-            if (connection.connected) {
-                connection.sendUTF(JSON.stringify(browsers2));
-            } else {
-                log.error("specSucces failed to use connection")
-            }
-    };
-
-    this.onBrowserComplete = function (browser) {
-            if (connection.connected) {
-                connection.sendUTF(JSON.stringify({'browserComplete': browser.id}));
-            } else {
-                log.error("specSucces failed to use connection")
-            }
-        log.info("onBrowserComplete: " + browser)
-    };
-
-    this.onRunComplete = function () {
-            if (connection.connected) {
-                connection.sendUTF(JSON.stringify({'runComplete': true}));
-            } else {
-                log.error("specSucces failed to use connection")
-            }
-        log.info("onRunComplete ")
-    };
+  this.onRunComplete = function () {
+    sendData({ 'type': 'runComplete' });
+    log.info("onRunComplete ")
+  };
 
 
-    this.specSuccess = this.specSkipped = this.specFailure = function (browser, result) {
-            if (connection.connected) {
-                log.info("specSucces will be send to client")
-                connection.sendUTF(JSON.stringify({'specResult': JSON.stringify(result)}));
-            } else {
-                log.error("specSucces failed to use connection")
-            }
-        log.info("specSuccess")
-    };
+  this.specSuccess = this.specSkipped = this.specFailure = function (browser, result) {
+    sendData(JSON.stringify({'type': 'test', 'result': result, 'browserId': browser.id }));
+    log.info("specSuccess")
+  };
 
-    this.onExit =  function (done) {
-        log.info("onExit");
-        done();
-    };
+  this.onExit = function (done) {
+    log.info("onExit");
+    done();
+  };
 
 };
 
 RemoteReporter.$inject = ['baseReporterDecorator', 'config.remoteReporter', 'emitter', 'logger',
-    'helper', 'formatError'];
+  'helper', 'formatError'];
 
 // PUBLISH DI MODULE
 module.exports = {
-    'reporter:remote': ['type', RemoteReporter]
+  'reporter:remote': ['type', RemoteReporter]
 };
---
+++ test/lib/testwebsocketserver.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ohoogvliet
+ * Date: 16.08.13
+ * Time: 16:13
+ * To change this template use File | Settings | File Templates.
+ */
