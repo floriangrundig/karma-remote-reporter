97446ed034649a8bc347e72beb142bc59a0372fa
ohoogvliet <oliver@hoogvliet.de>
2013-08-17 17:55:26 +0200

    Improved development script

--- scripts/development.sh
+++ scripts/development.sh
@@ -18,7 +18,15 @@ npm install websocket
 npm install $HOME_DIR/karma-remote-reporter-0.0.1.tgz
 ln -s $HOME_DIR/test
 
-echo "Finished"
+cd TEST_ENV/node_modules/karma-remote-reporter
+mv index.js index.js.orig
+
+// DANGEROUS: this must be a hard link to work. Feels ugly...
+ln $HOME_DIR/index.js
+
+cd TEST_ENV
+
+echo "Finished. You are now in the test environment folder."
 
 echo "Now Start the test server: node test/lib/testwebsocketserver.js"
 
@@ -27,3 +35,9 @@ echo "Go to another terminal and do cd ${TEST_ENV}"
 echo "Then start karma: karma start test/lib/karma.conf.js"
 
 echo "All test results should also occur on output of the test server"
+
+echo "-------------"
+
+echo "For changes, go back to your checked-out project and edit those files."
+echo "They are linked to the test environment. For changes at the test server, "
+echo "you need to restart the node server."
