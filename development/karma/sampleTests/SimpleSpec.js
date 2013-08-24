97c246c1220ab90da7a97a7ef831751f676101ca
Florian Grundig <florian.grundig@gmx.de>
2013-08-24 13:04:25 +0200

    added tests after tagging for first version v.0.1.0

---
+++ development/karma/sampleTests/SimpleSpec.js
@@ -0,0 +1,11 @@
+describe('Sample Test', function () {
+
+  it('should say that something is true', function () {
+    expect(true).toBe(true);
+  });
+
+  it('should say that something failed', function () {
+    expect(true).toBe(false);
+  });
+
+});
